09314bbc74aec7ea5506b7481fa550183319a31b
safaex <133200932+safaex@users.noreply.github.com>
2023-10-17 11:13:09 +0200

    Add files via upload

--- web.php
+++ web.php
@@ -21,28 +21,3 @@ Route:: get('/hello', function () {
 
 
 
-
-Route:: get('/planets', function (){ 
-$planets = [
-    [
-        'name' => 'Mars',
-        'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
-    ],
-    [
-        'name' => 'Venus',
-        'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
-    ],
-    [
-        'name' => 'Earth',
-        'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
-    ],
-    [
-        'name' => 'Jupiter',
-        'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
-    ],
-];
-return view('test')->with(['planets' => $planets]);
-
-});
-
-?>   
